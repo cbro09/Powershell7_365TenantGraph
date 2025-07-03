f3e8cd35553ce34544170d0215b5393aa29453ea
Callum <cbrodie09@outlook.com>
2025-07-03 08:35:34 +0100

    Group Module v2

--- Groups-Module-PS7.ps1
+++ Groups-Module-PS7.ps1
@@ -458,5 +458,6 @@ function Test-GroupCreationPrerequisites {
     return ($prerequisites.Values -notcontains $false)
 }
 
+
 # === Export Functions ===
 Export-ModuleMember -Function New-TenantGroups, Test-GroupCreationPrerequisites
